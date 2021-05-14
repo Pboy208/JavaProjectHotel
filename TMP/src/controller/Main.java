155fbeced1a088077f08a6767ee97399fce051bd
Pboy208 <73631714+phuonghoang2k@users.noreply.github.com>
2021-05-14 13:23:14 +0700

    tmp

--- TMP/src/controller/Main.java
+++ TMP/src/controller/Main.java
@@ -10,14 +10,15 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("Login.fxml"));
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
 			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("/view/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		System.out.println("start Running");
 	}
 
 	public static void main(String[] args) {
