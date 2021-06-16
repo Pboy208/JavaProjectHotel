442dd02a273fa9da489a66db176ba3ad50af5aa5
Pboy208 <73631714+phuonghoang2k@users.noreply.github.com>
2021-06-16 18:27:41 +0700

    update for tuanminh

--- HotelBookingDB-StoredProcedure/database/DB.sql
+++ HotelBookingDB-StoredProcedure/database/DB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th6 16, 2021 lúc 12:54 PM
+-- Thời gian đã tạo: Th6 16, 2021 lúc 12:57 PM
 -- Phiên bản máy phục vụ: 10.4.18-MariaDB
 -- Phiên bản PHP: 8.0.5
 
